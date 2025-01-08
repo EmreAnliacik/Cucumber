cc43a61adc83210b206caac771340ca8d8fe4714
EmreAnliacik <emreanliacik@hotmail.com>
2025-01-08 11:59:23 +0100

    Framework prep Parent Class

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import StepDefinitions.Parent;
 import Utilities.GWD;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -11,7 +12,7 @@ import org.testng.Assert;
 
 import java.time.Duration;
 
-public class DialogContent {
+public class DialogContent extends Parent {
 
 
     public DialogContent() {
@@ -34,48 +35,7 @@ public class DialogContent {
 
 
 
-    public void myClick(WebElement element){
 
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-
-        //scroll yap
-        scrollToElement(element);
-
-        element.click();
-
-    }
-
-
-    public void mySendKeys(WebElement element,String text){
-
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(element));
-
-        //scroll yap
-        scrollToElement(element);
-        element.clear();
-        element.sendKeys(text);
-
-    }
-
-    public void scrollToElement(WebElement element){
-
-        JavascriptExecutor js = (JavascriptExecutor)GWD.getDriver();
-        js.executeScript("arguments[0].scrollIntoView();", element);
-
-
-    }
-
-
-    public void verifyContainsText(WebElement elementm,String text) {
-
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.textToBePresentInElement(elementm, text));
-
-
-        Assert.assertTrue(elementm.getText().contains(text));
-    }
 
 
 
---
+++ src/test/java/StepDefinitions/Parent.java
@@ -0,0 +1,59 @@
+package StepDefinitions;
+
+import Utilities.GWD;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Parent {
+
+    public void myClick(WebElement element){
+
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+
+        //scroll yap
+        scrollToElement(element);
+
+        element.click();
+
+    }
+
+
+    public void mySendKeys(WebElement element,String text){
+
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+        //scroll yap
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(text);
+
+    }
+
+    public void scrollToElement(WebElement element){
+
+        JavascriptExecutor js = (JavascriptExecutor)GWD.getDriver();
+        js.executeScript("arguments[0].scrollIntoView();", element);
+
+
+    }
+
+
+    public void verifyContainsText(WebElement elementm,String text) {
+
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.textToBePresentInElement(elementm, text));
+
+
+        Assert.assertTrue(elementm.getText().contains(text));
+    }
+
+
+
+}
